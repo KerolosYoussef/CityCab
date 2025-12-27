4edc5ad5133cab84d63619d6ef72579eab3f224e
KerolosYoussef <kerolosyoussef2021@gmail.com>
2025-12-27 20:24:49 +0200

    add: validation behavior to run all validators dynamically

---
+++ src/BuildingBlocks/CityCab.Common/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,32 @@
+﻿using CityCab.Common.Interfaces.CQRS.Commands;
+using FluentValidation;
+using MediatR;
+
+namespace CityCab.Common.Behaviors
+{
+    public class ValidationBehavior<TRequest, TResponse>(IEnumerable<IValidator<TRequest>> validators) 
+        : IPipelineBehavior<TRequest, TResponse> where TRequest : ICommand<TResponse>
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators = validators;
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            var context = new ValidationContext<TRequest>(request);
+
+            var validationResults =
+                await Task.WhenAll(_validators.Select(v => v.ValidateAsync(context, cancellationToken)).ToList());
+
+            var failures =
+                validationResults.Where(vr => vr.Errors.Any())
+                .SelectMany(vr => vr.Errors)
+                .ToList();
+
+            if (failures.Any())
+            {
+                throw new ValidationException(failures);
+            }
+
+            return await next(cancellationToken);
+        }
+    }
+}
