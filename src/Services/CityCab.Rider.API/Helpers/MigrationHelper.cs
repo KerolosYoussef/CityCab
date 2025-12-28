81b844fc735ae8403bd148057cdea95a5f33ba84
KerolosYoussef <kerolosyoussef2021@gmail.com>
2025-12-28 17:46:18 +0200

    renamed the SeedData class to more convenient name (MigrationHelper)

---
+++ src/Services/CityCab.Rider.API/Helpers/MigrationHelper.cs
@@ -0,0 +1,22 @@
+﻿namespace CityCab.Rider.API.Helpers
+{
+    public static class MigrationHelper
+    {
+        public static async Task MigrateData(this IServiceProvider serviceProvider)
+        {
+            using var scope = serviceProvider.CreateScope();
+            var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+            try
+            {
+                logger.LogInformation("Migrating database...");
+                var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                await context.Database.MigrateAsync();
+                logger.LogInformation("Database migrated successfully");
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "An error occurred while migrating database");
+            }
+        }
+    }
+}
