f7703f812cfd64d74b682aaeb720b07e5d6ac08d
KerolosYoussef <kerolosyoussef2021@gmail.com>
2025-12-28 17:45:34 +0200

    enhanced: validation behaviors class

--- src/BuildingBlocks/CityCab.Common/Behaviors/ValidationBehavior.cs
+++ src/BuildingBlocks/CityCab.Common/Behaviors/ValidationBehavior.cs
@@ -7,21 +7,19 @@ namespace CityCab.Common.Behaviors
     public class ValidationBehavior<TRequest, TResponse>(IEnumerable<IValidator<TRequest>> validators) 
         : IPipelineBehavior<TRequest, TResponse> where TRequest : ICommand<TResponse>
     {
-        private readonly IEnumerable<IValidator<TRequest>> _validators = validators;
-
         public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
         {
             var context = new ValidationContext<TRequest>(request);
-
+                
             var validationResults =
-                await Task.WhenAll(_validators.Select(v => v.ValidateAsync(context, cancellationToken)).ToList());
+                await Task.WhenAll(validators.Select(v => v.ValidateAsync(context, cancellationToken)).ToList());
 
             var failures =
-                validationResults.Where(vr => vr.Errors.Any())
+                validationResults.Where(vr => vr.Errors.Count > 0)
                 .SelectMany(vr => vr.Errors)
                 .ToList();
 
-            if (failures.Any())
+            if (failures.Count > 0)
             {
                 throw new ValidationException(failures);
             }
